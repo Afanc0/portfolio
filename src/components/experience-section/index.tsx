b25991cc0f4d1e34cdb73863d132183800daaa92
afanc0 <gianromani.gr@gmail.com>
2025-06-22 23:43:09 -0400

    mobile compa

--- src/components/experience-section/index.tsx
+++ src/components/experience-section/index.tsx
@@ -7,13 +7,13 @@ export const ExperienceSection = () => {
     return (
         <Section id="experience" name="Experience" className="flex flex-col p-3 gap-8">
             {data.map((val, key) => (
-                <div className="p-3 flex flex-row gap-3 flex-wrap" key={key}>
-                    <div className="p-3 flex flex-col gap-3 flex-1 min-w-96">
+                <div className="p-3 flex flex-col lg:flex-row gap-3 flex-wrap" key={key}>
+                    <div className="p-3 flex flex-col gap-3 flex-1">
                         <span className="font-light">{val.time}</span>
                         <span className="font-light text-4xl">{val.title}</span>
                         <span className="font-medium">{val.company}</span>
                     </div>
-                    <div className="p-3 flex flex-col gap-3 flex-1 min-w-96">
+                    <div className="p-3 flex flex-col gap-3 flex-1">
                         <span className="font-light">{val.summary}</span>
                         <div className="flex gap-3 min-w-full flex-wrap">
                             {val.skills.map((skill, skillKey) => (
